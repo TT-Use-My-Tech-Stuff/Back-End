810f67afb844e44b97d0d8cd3627f0322bbb7514
Weston Woodard <westonwoodard28@gmail.com>
2021-04-24 17:17:46 -0700

    database created

--- data/migrations/20210424235440_project-database.js
+++ data/migrations/20210424235440_project-database.js
@@ -1,8 +1,37 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema
+    .createTable('users', tbl => {
+        tbl.increments('user_id')
+        tbl.string('username', 128)
+            .unique()
+            .notNullable()
+        tbl.string('password', 128)
+            .notNullable()
+    })
+    .createTable('equipment', tbl => {
+        tbl.increments('equipment_id')
+        tbl.string('equipment_name', 128)
+            .notNullable()
+        tbl.string('equipment_description')
+        tbl.integer('owner_id')
+            .notNullable()
+            .unsigned()
+            .references('user_id')
+            .inTable('users')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+        tbl.integer('renter_id')
+            .unsigned()
+            .references('user_id')
+            .inTable('users')
+            .onUpdate('RESTRICT')
+            .onDelete('RESTRICT')
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+    .dropTableIfExists('equipment')
+    .dropTableIfExists('users')
 };
