edb6f924cf5ddbf1b09461c89de1dfe637004898
Weston Woodard <westonwoodard28@gmail.com>
2021-04-26 11:01:20 -0700

    added env file

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
     migrations: { directory: './data/migrations'},
     pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) }, // allows for foreign key use in sqlite3
     connection: { filename: './data/project.db3'},
-    seeds: { directory: './data/seeds'} // no seeds needed at this time
+    seeds: { directory: './data/seeds'}
   }
 
 
