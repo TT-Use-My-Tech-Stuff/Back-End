35ee165e055c5f9ea59e76f07e9a815f1309c931
Weston Woodard <westonwoodard28@gmail.com>
2021-04-26 11:13:18 -0700

    added production to knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -8,6 +8,15 @@ module.exports = {
     pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) }, // allows for foreign key use in sqlite3
     connection: { filename: './data/project.db3'},
     seeds: { directory: './data/seeds'}
+  },
+
+  production: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    migrations: { directory: './data/migrations'},
+    pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) }, // allows for foreign key use in sqlite3
+    connection: { filename: './data/project.db3'},
+    seeds: { directory: './data/seeds'}
   }
 
 
