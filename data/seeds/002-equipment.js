8b06bffe6587a004e4910cd2e57e46d15d404f02
Weston Woodard <westonwoodard28@gmail.com>
2021-04-27 10:39:49 -0700

    changed seed

--- data/seeds/002-equipment.js
+++ data/seeds/002-equipment.js
@@ -5,8 +5,8 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('equipment').insert([
-        {equipment_name: 'camera1', equipment_description: 'this is a test description for equipment 1', owner_id: 2, renter_id: null},
-        {equipment_name: 'VCR2', equipment_description: 'this is a test description for equipment 2', owner_id: 2, renter_id: null},
+        {equipment_name: 'camera1', equipment_description: 'this is a test description for equipment 1', owner_id: 2, renter_id: 1},
+        {equipment_name: 'VCR2', equipment_description: 'this is a test description for equipment 2', owner_id: 2, renter_id: 1},
         {equipment_name: 'PC3', equipment_description: 'this is a test description for equipment 3', owner_id: 2, renter_id: null},
         {equipment_name: 'camera4', equipment_description: 'this is a test description for equipment 4', owner_id: 3, renter_id: null},
         {equipment_name: 'VCR5', equipment_description: 'this is a test description for equipment 5', owner_id: 3, renter_id: null}
